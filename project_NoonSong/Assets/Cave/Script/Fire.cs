bd60201f5fd5741467f19bebc259f76ada937971
lizuAg <wndms058@gmail.com>
2020-08-27 20:53:20 +0900

    Cave 씬 추가
    동굴 미니게임.

---
+++ project_NoonSong/Assets/Cave/Script/Fire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Transform enemy_prefab;
+
+    void Update()
+    {
+        if (transform.position.y > 20)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Vector2 pos = other.gameObject.transform.position;
+            Destroy(other.gameObject);
+            Instantiate(enemy_prefab, new Vector2(pos.x, 20), Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ project_NoonSong/Assets/Cave/Script/initScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initScene : MonoBehaviour
+{
+    public Transform enemy_prefab;
+
+    void Awake()
+    {
+        for(int i = -4; i < 5; i++)
+        {
+            Instantiate(enemy_prefab, new Vector2(i * 3.0f, 8), Quaternion.identity);
+        }
+    }
+}
