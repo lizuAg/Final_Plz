940efd871006065705a373b218150b13b6ad24a9
leeseooo <leeseoooo@naver.com>
2020-11-01 19:57:05 +0900

    11/1
    지하철 99%완성했고 눈송집 엔딩장면 넣고 있는 중이었음

---
+++ project_NoonSong/Assets/Cave/Script/initScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class initScene : MonoBehaviour
+{
+    public Transform enemy_prefab;
+    public Text batText;
+    public Text timeText;
+    public static int batNum=0;
+    public float timer;
+
+    void Awake()
+    {
+        for(int i = -4; i < 5; i++)
+        {
+            Instantiate(enemy_prefab, new Vector2(i * 3.0f, 8), Quaternion.identity);
+        }
+    }
+    void Start()
+    {
+        batText.text = "잡은 박쥐 수: " + batNum + "/30";//숫자는 조정.
+        timeText.text = "남은 시간: " + 30+" 초";
+    }
+    void FixedUpdate()
+    {
+        batText.text = "잡은 박쥐 수: " + batNum + "/30";//숫자는 조정.
+        timeText.text = "남은 시간: " + (30-(int)timer)+" 초";
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 30)
+        {
+            if (batNum > 50)
+                Debug.Log("박쥐를 물리치고 동굴을 탈출했다!");
+            Debug.Log("흠... 어떻게 해야하지~");
+        }
+    }
+}
--- project_NoonSong/Assets/Room/Script/ChangeImg.cs
+++ project_NoonSong/Assets/Room/Script/ChangeImg.cs
@@ -8,6 +8,10 @@ public class ChangeImg : MonoBehaviour
     public int EndingNumber=0;
     public Sprite 지하철의자;
     public Sprite 지하철인파;
+<<<<<<< Updated upstream
+=======
+    public Sprite 지하철꼰대;
+>>>>>>> Stashed changes
     public Sprite 교수;
     public Sprite 교통사고;
     public Sprite 바나나;
@@ -22,6 +26,7 @@ public class ChangeImg : MonoBehaviour
     public Sprite 푸들;
     public Sprite 피시방;
     public Sprite 휴강;
+<<<<<<< Updated upstream
 
     public void Change()
     {
@@ -29,5 +34,50 @@ public class ChangeImg : MonoBehaviour
             gameObject.GetComponent<Image>().sprite = 지하철의자;
         else if(EndingNumber == 6)
             gameObject.GetComponent<Image>().sprite = 지하철인파;
+=======
+    public Sprite 버블티;
+    public Sprite 돌부리;
+    public Sprite 횡단보도;
+
+    public void Change()
+    {
+        if (EndingNumber == 2)
+            gameObject.GetComponent<Image>().sprite = 돌부리;
+        else if (EndingNumber == 3)
+            gameObject.GetComponent<Image>().sprite = 바나나;
+
+        else if (EndingNumber == 5)
+            gameObject.GetComponent<Image>().sprite = 지하철의자;
+
+        else if (EndingNumber == 6)
+            gameObject.GetComponent<Image>().sprite = 지하철인파;
+
+        else if (EndingNumber == 7)
+            gameObject.GetComponent<Image>().sprite = 지하철꼰대;
+
+        else if (EndingNumber == 8)
+            gameObject.GetComponent<Image>().sprite = 비둘기;
+
+        else if (EndingNumber == 9)
+            gameObject.GetComponent<Image>().sprite = 교수;
+
+        else if (EndingNumber == 14)
+            gameObject.GetComponent<Image>().sprite = 휴강;
+
+        else if (EndingNumber == 16)
+            gameObject.GetComponent<Image>().sprite = 킥보드;
+
+        else if (EndingNumber == 24)
+            gameObject.GetComponent<Image>().sprite = 피시방;
+
+        else if (EndingNumber == 25)
+            gameObject.GetComponent<Image>().sprite = 벚꽃엔딩;
+
+        else if (EndingNumber == 26)
+            gameObject.GetComponent<Image>().sprite = 버블티;
+        else if (EndingNumber == 27)
+            gameObject.GetComponent<Image>().sprite = 횡단보도;
+
+>>>>>>> Stashed changes
     }
 }
